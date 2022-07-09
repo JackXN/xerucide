4be60457308c2ec1cb6b552a31907f5147daae1d
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2022-07-09 11:00:19 -0600

    initial deployment

---
+++ Components/Nav/index.js
@@ -0,0 +1,67 @@
+import React from "react";
+import { Box, Text, Link, StylesProvider } from "@chakra-ui/react";
+
+const navItems = [
+  {
+    id: 1,
+    label: "Home",
+  },
+  {
+    id: 2,
+    label: "Contact",
+  },
+  {
+    id: 3,
+    label: "Store",
+  },
+  {
+    id: 4,
+    label: "Promotions",
+  },
+];
+
+const index = () => {
+  return (
+    <>
+      <Box sx={styles.navContainer} className='glitchTwo '>
+          {navItems.map((item,index) => (
+              <Box key={item.id} sx={styles.linkItems} >
+              <Link key={item.id} target='_blank' >{item.label}</Link>
+            </Box>
+          ))}
+      </Box>
+    </>
+  );
+};
+
+export default index;
+
+const styles = {
+  navContainer: {
+    display: ['none', 'flex', 'flex'],
+    // background: '#ffff',
+      transform: 'rotate(270deg)',
+      position: 'relative',
+      zIndex: '10000000',
+      // opacity: '0.2'
+      
+  },
+  linkItems: {
+    ml:'20px',
+    mr:'20px',
+    mt: '20px',
+    mb: '20px',
+    fontSize: ['20px'],
+    fontFamily:'Xenon',
+   color:'white',
+   textUnderline: 'false',
+
+   '&:hover': {
+     cursor: 'pointer',
+      
+   }
+
+   
+  }
+
+};
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,32 @@
-import '../styles/globals.css'
+import '../styles/globals.scss';
+import {ChakraProvider} from '@chakra-ui/react';
+import {extendTheme} from '@chakra-ui/react';
+
+
+const theme = extendTheme({
+  colors: {
+    brand: {
+      100: "#f7fafc",
+      // ...
+      900: "blue",
+    },
+    primary: {
+    'orange': '#F47F39',
+      'gray': '#787878',
+      'white': '#fffff'
+    }
+  },
+})
+
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <ChakraProvider theme={theme}>
+ <Component {...pageProps} />
+  </ChakraProvider>
+)
 }
+
 
 export default MyApp
